de8f9942ba726372809df73d8a7bef5d968e0f1c
ohiovalleyna <webmaster@ohiovalleyna.org>
2019-03-31 13:03:26 -0400

    Removed extra logging from address pipe

--- src/app/pipes/addresslink/addresslink.pipe.ts
+++ src/app/pipes/addresslink/addresslink.pipe.ts
@@ -7,10 +7,6 @@ import { Address } from 'src/app/models/meeting';
 export class AddresslinkPipe implements PipeTransform {
 
   transform(address: Address, args?: any): any {
-    console.log(address.street.split(" "));
-    console.log(address.city.split(" "));
-    console.log(address.state.split(" "));
-    console.log(address.zip.split(" "));
     var addressArray = (address.street.split(" ") || []);
     addressArray.push(...(address.city.split(" ")   || []));
     addressArray.push(...(address.state.split(" ")  || []));
